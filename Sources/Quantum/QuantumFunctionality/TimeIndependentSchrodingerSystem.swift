19c00d5f4cc6b104cae5766ddb22a2e19125a7d7
George Tait <george.tait512@gmail.com>
2024-03-20 17:45:28 +0000

    TimeIndependantSchrodingerSystem Changes
    Added a quick hack to allow for sparse types here. This is not good practice.
    This is for the use of benchmarking
    
    also created a simulate function with similar functionality to the qutip.sesolve function

--- Sources/Quantum/QuantumFunctionality/TimeIndependentSchrodingerSystem.swift
+++ Sources/Quantum/QuantumFunctionality/TimeIndependentSchrodingerSystem.swift
@@ -75,3 +75,151 @@ open class TimeIndependentSchrodingerSystem {
     }
 }
 //  Created by M J Everitt on 21/01/2022.
+
+
+/*Quick hack. making equivalent structures for each of the types conforming to OperatorType so far. That is, Matrix, SparseMatrix and DiagonalSparseMatrix.
+ Note, this is NOT good practice.
+*/
+
+
+open class TimeIndependentSchrodingerSparseSystem {
+    public var Psi: StateVector
+    public var minus_i_H: SparseMatrix<ComplexReal>
+    public var time: Real
+    
+    public init(initialstate: StateVector,
+                hamiltonian: SparseMatrix<ComplexReal>) {
+        let minusi = ComplexReal( real: 0.0, imag: -1.0 )
+        time = 0.0
+        Psi = initialstate
+        minus_i_H = minusi * hamiltonian
+    }
+    
+    open func schrodingerEquation(time: Real, psi: StateVector)-> StateVector {
+            
+      return minus_i_H * psi
+        
+      
+            
+    }
+
+    public func evolve(by dt: Real) {
+        
+        multiStepIvpIntegrator(from: time,
+                               to: time + dt,
+                               first_try_of_stepsize: dt,
+                               smallest_allowed_value_of_stepsize: 1.0e-8,
+                               accuracy: 10e-6,
+                               y: &Psi,
+                               derivative_function: schrodingerEquation)
+        
+        time += dt
+        
+        /*
+            More sophisticated versions of this class might allow one to choose which
+            integrator to use as we could replace the above with e.g.
+         
+                  Psi = doRungeKuttaStep(t: time,
+                                         h: dt,
+                                         y: Psi,
+                                         derivative_function: schrodingerEquation)
+        */
+    }
+    
+    
+    public func simulate(times: [Real], takeExpectationOf qoperators: SparseMatrix<ComplexReal>...) -> [[ComplexReal]] {
+        
+        var expectationValues = [[ComplexReal]](repeating: [ComplexReal](repeating: ComplexReal(0), count: times.count), count: qoperators.count)
+        
+        for i in 0..<times.count-1 {
+            
+            for j in 0..<qoperators.count {
+                expectationValues[j][i] = qoperators[j].expectationValue(of: Psi)
+            }
+            
+            evolve(by: times[i+1]-times[i])
+            
+        }
+        
+        for j in 0..<qoperators.count {
+            expectationValues[j][times.count-1] = qoperators[j].expectationValue(of: Psi)
+            
+        }
+        
+        return expectationValues
+        
+        
+    }
+}
+
+
+open class TimeIndependentSchrodingerDiagonalSparseSystem {
+    public var Psi: StateVector
+    public var minus_i_H: DiagonalSparseMatrix<ComplexReal>
+    public var time: Real
+    
+    public init(initialstate: StateVector,
+                hamiltonian: DiagonalSparseMatrix<ComplexReal>) {
+        let minusi = ComplexReal( real: 0.0, imag: -1.0 )
+        time = 0.0
+        Psi = initialstate
+        minus_i_H = minusi * hamiltonian
+    }
+    
+    open func schrodingerEquation(time: Real, psi: StateVector)-> StateVector {
+            
+      return minus_i_H * psi
+        
+      
+            
+    }
+
+    public func evolve(by dt: Real) {
+        
+        multiStepIvpIntegrator(from: time,
+                               to: time + dt,
+                               first_try_of_stepsize: dt,
+                               smallest_allowed_value_of_stepsize: 1.0e-8,
+                               accuracy: 10e-6,
+                               y: &Psi,
+                               derivative_function: schrodingerEquation)
+        
+        time += dt
+        
+        /*
+            More sophisticated versions of this class might allow one to choose which
+            integrator to use as we could replace the above with e.g.
+         
+                  Psi = doRungeKuttaStep(t: time,
+                                         h: dt,
+                                         y: Psi,
+                                         derivative_function: schrodingerEquation)
+        */
+    }
+    
+    
+    public func simulate(times: [Real], takeExpectationOf qoperators: DiagonalSparseMatrix<ComplexReal>...) -> [[ComplexReal]] {
+        
+        var expectationValues = [[ComplexReal]](repeating: [ComplexReal](repeating: ComplexReal(0), count: times.count), count: qoperators.count)
+        
+        for i in 0..<times.count-1 {
+            
+            for j in 0..<qoperators.count {
+                expectationValues[j][i] = qoperators[j].expectationValue(of: Psi)
+            }
+            
+            evolve(by: times[i+1]-times[i])
+            
+        }
+        
+        for j in 0..<qoperators.count {
+            expectationValues[j][times.count-1] = qoperators[j].expectationValue(of: Psi)
+            
+        }
+        
+        return expectationValues
+        
+        
+    }
+}
+
